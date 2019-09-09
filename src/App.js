41545bc5b3f4d43ac0507c4bdb19b0d8a68466b0
Lucas Bazemore <lucasabazemore@gmail.com>
2019-09-09 12:12:30 -0500

    refactor navigation and shopping cart to use context

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import data from './data';
 
 //Context
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -20,17 +21,16 @@ function App() {
 
     return (
         <ProductContext.Provider value={{ products, addItem }}>
-            <div className="App">
-                <Navigation cart={cart} />
+            <CartContext.Provider value={cart}>
+                <div className="App">
+                    <Navigation cart={cart} />
 
-                {/* Routes */}
-                <Route exact path="/" component={<Products />} />
+                    {/* Routes */}
+                    <Route exact path="/" component={Products} />
 
-                <Route
-                    path="/cart"
-                    render={() => <ShoppingCart cart={cart} />}
-                />
-            </div>
+                    <Route path="/cart" component={ShoppingCart} />
+                </div>
+            </CartContext.Provider>
         </ProductContext.Provider>
     );
 }
